9a69f8f6c8f356fbb5f6dd6ff3d3936981a27c00
Colin Bruce <colinbruce@gmail.com>
2014-12-14 22:27:39 +0000

    removed obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get '/', to: 'anagram#index', as: :root
 
   get '/anagram/index'
-  get '/help', to: 'anagram#help'
 
   get '/:wordlist', to: 'anagram#search'
 end
