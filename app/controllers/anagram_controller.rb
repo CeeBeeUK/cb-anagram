5d7de79897df3c7ccf9c48ee3170285c7ef7cd04
Colin Bruce <colinbruce@gmail.com>
2014-12-14 21:54:46 +0000

    added dictionary load and test

--- app/controllers/anagram_controller.rb
+++ app/controllers/anagram_controller.rb
@@ -5,8 +5,14 @@ class AnagramController < ApplicationController
 
   def search
     words = params['wordlist'].split(',')
-    puts words
-    render :text => words.to_json
+    result={}
+    words.each do |word|
+      matches = get_matches(get_dictionary, word)
+
+      result["#{word}"] = matches
+    end
+
+    render :text => result.to_json
   end
 
   def help
@@ -19,4 +25,21 @@ private
 
 def word_list_params
   params.require(:wordlist)
+end
+
+def get_dictionary
+  word_source = File.join('lib','assets','words.txt')
+  dict = File.open(word_source, 'r'){|file| file.readlines.collect{|line| line.chomp}}
+  dict
+end
+
+
+def get_matches(dictionary, word)
+  matches = []
+  a = word.chars.sort
+  dictionary.each do |w|
+    test_word = w.chars.sort
+    matches << w if test_word == a && w!=word
+  end
+  matches
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'anagram#index', as: :root
 
-  get 'anagram/index'
-  
+  get '/anagram/index'
+  get '/help', to: 'anagram#help'
+
+  get '/:wordlist', to: 'anagram#search'
 end
