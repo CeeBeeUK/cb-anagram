a48e6094927e066b8345e40ca9507b45cf0e68b2
Colin Bruce <colinbruce@gmail.com>
2014-12-14 22:11:33 +0000

    added tests for JSON return

--- spec/controllers/anagram_controller_spec.rb
+++ spec/controllers/anagram_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe AnagramController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+  describe 'GET search' do
+    it 'should return http success' do
+      get :search, :wordlist => 'stop'
+      expect(response).to have_http_status(:success)
+    end
+    it 'should return valid json' do
+      get :search, :wordlist => 'stop'
+      expect(response.content_type).to eq("application/json")
+    end
+    it 'should return multiple json objects' do
+      expected = JSON.parse('{"stop":["opts","post","pots","spot","tops"],"start":["tarts"]}')
+      get :search, :wordlist => 'stop,start'
+      expect(response.content_type).to eq("application/json")
+      expect(JSON.parse(response.body)).to eql(expected)
+    end
+  end
 end
